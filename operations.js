6e5de8e6500c2b3a475a2f1e515bc160256da432
Debiprasadnandi <danna.debiprasad2005@gmail.com>
2025-10-29 21:24:44 +0530

    many types of operations

---
+++ operations.js
@@ -0,0 +1,24 @@
+let value = 3;
+let negvalue = -value;
+
+console.log(negvalue);
+
+let str1 = "hello";
+let str2 = "rocky";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); // "12"
+console.log(1 + "2"); // "12"
+console.log("1" +2 +2); // "122"
+console.log(2 +2 + "1"); // "41"
+
+console.log((3+4)*5%3);
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
