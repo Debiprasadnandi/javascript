081b9d836948948e07d63863c75629d9f038aa35
Debiprasadnandi <danna.debiprasad2005@gmail.com>
2025-10-29 10:44:15 +0530

    first commit

---
+++ 02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) we are using nodejs , not browser
+
+console.log(typeof 4.5)
+console.log(typeof null)
+console.log(typeof "hello")
