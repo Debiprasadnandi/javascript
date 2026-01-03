43f3e78bbc11a37d9eadf155c3227e68596581a8
Debiprasadnandi <danna.debiprasad2005@gmail.com>
2026-01-03 20:03:43 +0530

    object part 2

---
+++ object_2.js
@@ -0,0 +1,16 @@
+// const tinderuser = new object1();
+const tinderuser = {};
+
+tinderuser.id = "123abc";
+tinderuser.name = "rocky";
+tinderuser.isLoggedIn = false;
+
+// console.log(tinderuser);
+
+const regularuser = {
+    email: "danna.debiprasad2005@gmail.com",
+    fullname:{
+        userfullname: "rocky nandi",
+    }
+}
+console.log(regularuser.fullname.userfullname); 
